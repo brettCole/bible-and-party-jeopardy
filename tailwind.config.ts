708d8b0cd9d4c2829e93011ee3231bb78e279201
brett.cole <acodercalledcole@gmail.com>
2024-01-23 17:47:40 -0500

    Initial commit with installed fonts and couple tsconfig additions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,27 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'jeopardyYellow': '#FFCC00'
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        gyparody: ['var(--font-gyparody']
+      }
     },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      'synthwave',
+      'aqua'
+    ],
+    darkTheme: 'synthwave'
+  },
 };
 export default config;
